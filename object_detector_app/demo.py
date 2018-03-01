574ea5613cd9d24f75a3582b90347e0b0e3453d9
Anshuman Patnaik <anshuman.patnaik2008@gmail.com>
2018-03-01 18:46:38 -0500

    more refactoring

--- object_detector_app/demo.py
+++ object_detector_app/demo.py
@@ -9,164 +9,17 @@ import tensorflow as tf
 
 from utils.app_utils import FPS, WebcamVideoStream
 from utils.misc_utils import is_using_tensorflow_gpu
+# I know the below is bad practice (importing *)
+# TODO: Right a proper configManager to load in defaults
+from utils.defaults import *
+from utils.worker_utils import MessageWorker, OutputImageStreamWorker, \
+                               ObjectDetectoResponseWorker
+
 from multiprocessing import Queue, Pool, Process
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
-from object_detection.worker_utils import MessageWorker
-from tts import PythonTTS, cmdLineTTS
 
-CWD_PATH = os.getcwd()
-
-# Path to frozen detection graph. This is the actual model that is used for the object detection.
-MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
-PATH_TO_CKPT = os.path.join(CWD_PATH, 'object_detection', MODEL_NAME, 'frozen_inference_graph.pb')
-
-# List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = os.path.join(CWD_PATH, 'object_detection', 'data', 'mscoco_label_map.pbtxt')
-
-NUM_CLASSES = 90
-
-# Loading label map
-label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
-                                                            use_display_name=True)
-category_index = label_map_util.create_category_index(categories)
-
-# Kitchen items
-allowed_classes = ['person','bottle','knife','spoon','fork','cup','bowl','dog']
-
-# Detect Threshold
-DETECT_THRESHOLD = 0.2
-
-
-def detect_objects(image_np, sess, detection_graph):
-    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-    image_np_expanded = np.expand_dims(image_np, axis=0)
-    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-
-    # Each box represents a part of the image where a particular object was detected.
-    boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-
-    # Each score represent how level of confidence for each of the objects.
-    # Score is shown on the result image, together with the class label.
-    scores = detection_graph.get_tensor_by_name('detection_scores:0')
-    classes = detection_graph.get_tensor_by_name('detection_classes:0')
-    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-
-    # Actual detection.
-    (boxes, scores, classes, num_detections) = sess.run(
-        [boxes, scores, classes, num_detections],
-        feed_dict={image_tensor: image_np_expanded})
-    # Wanted objects
-    class_box = {}
-    max_width = 0
-    # Only show boxes around these classes
-    for i in range(len(scores[0])):
-        if category_index[classes[0][i]]['name'] not in allowed_classes:
-            scores[0][i] = 0
-        else:
-            if scores[0][i] >= DETECT_THRESHOLD:
-                obj = category_index[classes[0][i]]['name']
-                if obj == 'person':
-                    box = np.squeeze(boxes[0][i])
-                    width = abs(box[3] - box[1])
-                    if width < max_width:
-                        continue
-                    max_width = width
-                class_box[obj] = np.squeeze(boxes[0][i])
-
-    # Visualization of the results of a detection.
-    vis_util.visualize_boxes_and_labels_on_image_array(
-        image_np,
-        np.squeeze(boxes),
-        np.squeeze(classes).astype(np.int32),
-        np.squeeze(scores),
-        category_index,
-        use_normalized_coordinates=True,
-        line_thickness=8,
-        min_score_thresh=DETECT_THRESHOLD)
-    return image_np, class_box
-
-
-def worker(input_q, output_q, request_q, message_q):
-    # Load a (frozen) Tensorflow model into memory.
-    detection_graph = tf.Graph()
-    with detection_graph.as_default():
-        od_graph_def = tf.GraphDef()
-        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-            serialized_graph = fid.read()
-            od_graph_def.ParseFromString(serialized_graph)
-            tf.import_graph_def(od_graph_def, name='')
-
-        sess = tf.Session(graph=detection_graph)
-
-    image = ''
-    mes = ''
-    fps = FPS().start()
-    out = cmdLineTTS
-
-    while True:
-        fps.update()
-        frame = input_q.get()
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image, objects = detect_objects(frame_rgb, sess, detection_graph)
-        # dont block if nothing is there
-        try:
-            obj = request_q.get(block=False)
-        except:
-            obj = None
-
-        if obj:
-            # try to build output message from objects
-            if 'person' not in objects:
-                msg = 'Cannot locate user.'
-            elif obj not in objects:
-                msg = 'Unable to locate %s in current view' %(obj)
-            else:
-                mid_p = (objects.get('person')[1] \
-                            + objects.get('person')[3])/2
-                mid_o = (objects.get(obj)[1] \
-                        + objects.get(obj)[1])/2
-                if mid_p < mid_o:
-                    msg = obj + ' is to your left'
-                else:
-                    msg = obj + ' is to your right'
-
-            print 'worker here'
-            message_q.put(msg)
-        output_q.put(image)
-
-    fps.stop()
-    sess.close()
-
-def request_worker(request_q, message_q, socket_connection):
-    while not socket_connection.conn:
-        socket_connection.conn, addr = socket_connection.s.accept()
-        print 'Waiting on mock connection from Google Assistant'
-        time.sleep(1)
-
-    while True:
-        # listen on socket for incoming messages
-        request_data = socket_connection.conn.recv(BUFFER_SIZE)
-
-        print request_data
-        obj = None
-        # parse the incoming message
-        for word in request_data.split():
-            if word in allowed_classes:
-                obj = word
-                break
-
-        if obj:
-            request_q.put(obj)
-            print obj
-
-        msg = message_q.get()
-        socket_connection.conn.send(msg)
-        print(msg)
-
-
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-src', '--source', dest='video_source', type=int,
                         default=0, help='Device index of the camera.')
@@ -177,7 +30,7 @@ if __name__ == '__main__':
     parser.add_argument('-num-w', '--num-workers', dest='num_workers', type=int,
                         default=1, help='Number of workers.')
     parser.add_argument('-q-size', '--queue-size', dest='queue_size', type=int,
-                        default=5, help='Size of the queue.')
+                        default=10, help='Size of the queue.')
     parser.add_argument('-obj','--object', dest='object', type=str,
                         default='bottle',help='Object to search for.')
     args = parser.parse_args()
@@ -206,39 +59,58 @@ if __name__ == '__main__':
     request_q = Queue(maxsize=args.queue_size)
     message_q = Queue(maxsize=args.queue_size)
 
-    pool = Pool(num_workers, worker, (input_q, output_q, request_q, message_q))
+    # output stream workers, required to visualize inference on the
+    # fly
+    output_image_stream_workers = []
+    for _ in xrange(num_workers):
+        worker = OutputImageStreamWorker(input_q, output_q)
+        worker.start()
+        output_image_stream_workers.append(worker)
 
-    # interface with google home/flask client
+    # workers required to interface with google home/flask client
     msg_worker = MessageWorker(request_q, message_q)
     msg_worker.start()
+    response_worker = ObjectDetectoResponseWorker(input_q, request_q, message_q)
+    response_worker.start()
 
     video_capture = WebcamVideoStream(src=args.video_source,
                                       width=args.width,
                                       height=args.height).start()
-    fps = FPS().start()
 
-    while True:  # fps._numFrames < 120
-        frame = video_capture.read()
-        input_q.put(frame)
-
-        t = time.time()
-        output_rgb = cv2.cvtColor(output_q.get(), cv2.COLOR_RGB2BGR)
-        cv2.imshow('Video', output_rgb)
-        fps.update()
-
-        # print('[INFO] elapsed time: {:.2f}'.format(time.time() - t))
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    fps.stop()
-    print('[INFO] elapsed time (total): {:.2f}'.format(fps.elapsed()))
-    print('[INFO] approx. FPS: {:.2f}'.format(fps.fps()))
-
-    msg_worker.join()
-    pool.terminate()
-    video_capture.stop()
+    # Parent event loop
     while True:
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        try:
+            frame = video_capture.read()
+            input_q.put(frame)
+
+            t = time.time()
+            output_rgb = cv2.cvtColor(output_q.get(), cv2.COLOR_RGB2BGR)
+            cv2.imshow('Video', output_rgb)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        except KeyboardInterrupt:
             break
+
+    # TODO: Make kill process a multiproc semaphore/lock/signal that all the
+    # processes can just interface with
+    msg_worker.kill_process = True
+    msg_worker.join()
+    response_worker.kill_process = True
+    response_worker.join()
+
+    # splitting the kill signal and join will lead to slightly more
+    # concurrency
+    for worker in output_image_stream_workers:
+        worker.kill_process = True
+
+    for worker in output_image_stream_workers:
+        worker.join()
+
+    video_capture.stop()
+
     cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
--- object_detector_app/utils/worker_utils.py
+++ object_detector_app/utils/worker_utils.py
@@ -1,35 +1,34 @@
+import cv2
 import multiprocessing
-from multiprocessing import Queue, Pool, Process
-from user_feedback_utils import UserFeedbackGenerator
+import socket
+import tensorflow as tf
 
-# TODO: Split globals up inot their own file ie. a ConfigManager of sorts
-# socket globals
-TCP_IP = '127.0.0.1'
-TCP_PORT = 1315
-BUFFER_SIZE = 1024
-ALLOWED_CLASSES = ['person','bottle','knife','spoon','fork','cup','bowl','dog']
+from defaults import *
+from multiprocessing import Queue, Pool, Process
+#from user_feedback_utils import UserFeedbackGenerator
+from object_detector_utils import ObjectDetector
 
 
 class MessageWorker(Process):
     '''
         TODO: add comments, its 2AM, i will add comments tomorrow
     '''
-    def __init__(self, request_q, message_q, **kwargs):
+    def __init__(self, request_q, message_q, *args, **kwargs):
         # pop out socket info or use global defined here
-        self.tcp_ip = kwargs.pop(tcp_ip, TCP_IP)
-        self.tcp_port = kwargs.pop(tcp_port, TCP_PORT)
-        self.buffer_size = kwargs.pop(buffer_size, BUFFER_SIZE)
+        self.tcp_ip = kwargs.pop('tcp_ip', TCP_IP)
+        self.tcp_port = kwargs.pop('tcp_port', TCP_PORT)
+        self.buffer_size = kwargs.pop('buffer_size', BUFFER_SIZE)
 
         # init parent with remaining kwargs
         # we want to do this init first so we can fail on the parent's
         # exception before having to throw our own exception after.
-        super(MessageWorker, self).__init__(**kwargs)
+        super(MessageWorker, self).__init__(*args, **kwargs)
         self.request_q = request_q
         self.message_q = message_q
 
         # also setup socket information here
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._socket.bind(tcp_ip, tcp_port)
+        self._socket.bind((self.tcp_ip, self.tcp_port))
         self._socket.listen(1)
         self._conn = None
 
@@ -40,6 +39,7 @@ class MessageWorker(Process):
         # Wait for incoming connection before doing anything
         self.wait_for_incoming_connection()
 
+        # Enter Event loop
         while not self.kill_process:
             request_data = self._conn.recv(self.buffer_size)
             print request_data
@@ -83,9 +83,100 @@ class MessageWorker(Process):
 
 
 class OutputImageStreamWorker(Process):
-    pass
+    '''
+        TODO: Add comments
+    '''
+    def __init__(self, img_input_q, img_output_q, *args, **kwargs):
+        super(OutputImageStreamWorker, self).__init__(*args, **kwargs)
+        self.input_img_q = img_input_q
+        self.output_img_q = img_output_q
+
+        # FOR SOME GOD DAMN REASON TENSORFLOW ISN'T FORK SAFE
+        # SO CAN'T INITIALIZE HERE, HAVE TO INIT AFTER THE PARENT
+        # HAS FORKED INTO CHILD...
+        self._object_detector = None
+
+        # Bool to kill event loop
+        self.kill_process = False
+
+    def run(self):
+        self._object_detector = ObjectDetector()
+
+        # Enter Event loop for worker
+        while not self.kill_process:
+            # grab most recent frame img_input_q and convert to RGB
+            # as expected by model
+            frame = self.input_img_q.get()
+	    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            # do inference for frame capture
+	    out_image = self._object_detector.detect_objects_visualize(frame_rgb)
+            self.output_img_q.put(frame_rgb)
+
+        # should do with __enter__ and __exit__ methods, but w.e for now
+        self._object_detector.cleanup()
 
 
-class ObjectDetectorWorker(Process):
-    pass
+class ObjectDetectoResponseWorker(Process):
+    def __init__(self, img_input_q, request_q, message_q, *args, **kwargs):
+        super(ObjectDetectoResponseWorker, self).__init__(*args, **kwargs)
+        self.input_img_q = img_input_q
+        self.request_q = request_q
+        self.message_q = message_q
 
+        # FOR SOME GOD DAMN REASON TENSORFLOW ISN'T FORK SAFE
+        # SO CAN'T INITIALIZE HERE, HAVE TO INIT AFTER THE PARENT
+        # HAS FORKED INTO CHILD...
+        self._object_detector = None
+
+        # Bool to kill event loop
+        self.kill_process = False
+
+    def run(self):
+	# Load object detector after parent process forks
+        self._object_detector = ObjectDetector()
+
+        # Enter Event loop for worker
+        while not self.kill_process:
+            # block until the MessageWorker puts something in the
+            # request_q
+            obj_to_find = self.request_q.get()
+
+            # grab most recent frame img_input_q and convert to RGB
+            # as expected by model
+            frame = self.input_img_q.get()
+	    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            # do inference for frame capture
+	    detected_objects = self._object_detector.detect_objects(frame_rgb)
+
+            # construct message that we should pass back to user based on
+            # detected objects
+            msg = self.build_msg(detected_objects)
+
+            # add msg to message_q
+            self.message_q.put(msg)
+
+        self._object_detector.cleanup()
+
+    def build_msg(self, obj_to_find, detected_objects):
+        msg = ''
+
+	if 'person' not in detected_objects:
+            msg = 'Cannot locate user.'
+	elif obj_to_find not in detected_objects:
+	    msg = 'Unable to locate %s in current view' %(obj_to_find)
+	else:
+	    mid_p = (detected_objects.get('person')[1] \
+			+ detected_objects.get('person')[3])/2
+	    mid_o = (detected_objects.get(obj)[1] \
+		    + detected_objects.get(obj)[1])/2
+	    if mid_p < mid_o:
+		msg = obj_to_find + ' is to your left'
+	    else:
+		msg = obj_to_find + ' is to your right'
+
+        return msg
+
+    def load_stationary_objects(self):
+        pass
