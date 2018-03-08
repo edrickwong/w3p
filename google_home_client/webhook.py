4a569a7e7a24da37a7184ec0c2eeee63ae9b296c
Justin Wong <jh8wong@edu.uwaterloo.ca>
2018-03-07 19:59:26 -0500

    Fixing issue where we could not hit the backend code more than once

--- google_home_client/webhook.py
+++ google_home_client/webhook.py
@@ -12,6 +12,9 @@ logging.getLogger('flask_assistant').setLevel(logging.DEBUG)
 app = Flask(__name__)
 assist = Assistant(app, '/')
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+
 @assist.action('greeting')
 def greet_and_start():
     speech = "Hi, how can I help you?"
@@ -21,14 +24,13 @@ def greet_and_start():
 @assist.action("object-to-detect")
 def detect_object(object):
     print(object)
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((TCP_IP, TCP_PORT))
-    s.send(object)
-    message = s.recv(BUFFER_SIZE)
-    # may want to use tell if we want the session to end
+    message = handle_detect_object(object)
     return ask(message)
 
+def handle_detect_object(object):
+    s.send(object)
+    message = s.recv(BUFFER_SIZE)
+    return message
 
 @assist.action("unknown-object")
 def handle_unknown_object():
@@ -36,4 +38,4 @@ def handle_unknown_object():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
