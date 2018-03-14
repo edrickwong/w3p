c4427f5304ac17d7f76d5f2fe5bbcd45f15c68c1
Justin Wong <jh8wong@edu.uwaterloo.ca>
2018-03-13 21:48:17 -0400

    Adding repeat

--- google_home_client/webhook.py
+++ google_home_client/webhook.py
@@ -8,6 +8,10 @@ TCP_IP = '127.0.0.1'
 TCP_PORT = 1315
 BUFFER_SIZE = 1024
 
+# Fixed responses
+GREETING = "Hi, how can I help you?"
+UNKNOWN_OBJ_RESP = "Sorry, I can only detect bottles right now. Please try a different object."
+
 logging.getLogger('flask_assistant').setLevel(logging.DEBUG)
 app = Flask(__name__)
 assist = Assistant(app, '/')
@@ -15,17 +19,21 @@ assist = Assistant(app, '/')
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 
+# keeps track of last message said by Google Home
+msg_stack = []
+
+def respond(msg):
+    msg_stack.append(msg)
+    return ask(msg)
+
 @assist.action('greeting')
 def greet_and_start():
-    speech = "Hi, how can I help you?"
-    return ask(speech)
-
+    return respond(GREETING)
 
 @assist.action("object-to-detect")
 def detect_object(object):
-    print(object)
     message = handle_detect_object(object)
-    return ask(message)
+    return respond(message)
 
 def handle_detect_object(object):
     s.send(object)
@@ -34,7 +42,12 @@ def handle_detect_object(object):
 
 @assist.action("unknown-object")
 def handle_unknown_object():
-    return ask("Sorry, I can only detect bottles right now. Please try a different object.")
+    return respond(UNKNOWN_OBJ_RESP)
+
+@assist.action("repeat")
+def repeat():
+    most_recent_msg = msg_stack[-1:][0]
+    return respond(most_recent_msg)
 
 
 if __name__ == '__main__':
