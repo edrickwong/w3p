df969d97298c5eaff332a5b197533afdcd48a0ff
Linda Wang <lindawang@Lindas-MacBook-Pro.local>
2018-03-18 16:18:15 -0400

    added trained model as comments to defaults.py file

--- object_detector_app/utils/defaults.py
+++ object_detector_app/utils/defaults.py
@@ -24,14 +24,14 @@ sys.path.insert(0, APP_FOLDER)
 ##  Defaults related to object detection inference
 ##  TODO: make this interchangble (so we can add in new models easily
 ###
-MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017' #'ssd_mobilenet_v1_coco_11_06_2017'
+MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017' #'model_2018_03_13' #'ssd_mobilenet_v1_coco_11_06_2017'
 LABEL_MAP = 'mscoco_label_map.pbtxt' #'label_map.pbtxt' #'mscoco_label_map.pbtxt'
 PATH_TO_CKPT = os.path.join(APP_FOLDER, 'object_detection', MODEL_NAME, 'frozen_inference_graph.pb')
 PATH_TO_LABELS = os.path.join(APP_FOLDER, 'object_detection', 'data', LABEL_MAP)
 DETECT_THRESHOLD = 0.4
 NUM_CLASSES = 90
 
-ALLOWED_CLASSES = ['person','bottle','knife','spoon','fork','cup','bowl','dog']
+ALLOWED_CLASSES = ['bottle','knife','spoon','fork','cup','bowl','dog', 'kettle']
 
 ###
 ##  Defaults related to socket and communication
